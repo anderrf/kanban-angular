c0fcd8908cd8d9935241b67117c74d8141baac61
anderrf <anderson_rocha_farias@outlook.com>
2021-10-18 14:53:35 -0300

    Creation of kanban component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { KanbanComponent } from './components/kanban/kanban.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: KanbanComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
