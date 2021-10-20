92b74a697d54cd872b368c76fdf0e079debf2724
anderrf <anderson_rocha_farias@outlook.com>
2021-10-20 10:31:43 -0300

    Addition of task on board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KanbanComponent } from './components/kanban/kanban.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { KanbanComponent } from './components/kanban/kanban.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/kanban/kanban.component.ts
+++ src/app/components/kanban/kanban.component.ts
@@ -1,4 +1,8 @@
+import { KanbanTask } from '../kanbanTask';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-kanban',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KanbanComponent implements OnInit {
 
-  constructor() { }
+  public taskRegister: FormGroup;
+  tasks: KanbanTask[];
+  taskSequence: number;
+
+  constructor(private fb: FormBuilder) {
+    
+   }
 
   ngOnInit(): void {
+    this.taskRegister = this.fb.group({
+      taskName: ['', [Validators.required, Validators.minLength(3)]]
+    });
+    this.tasks = [];
+    this.taskSequence = 0;
+  }
+
+  addTask(): void{
+    if(this.taskRegister.valid){
+      let task: KanbanTask = new KanbanTask(++this.taskSequence, this.taskRegister.value['taskName'], 'to-do');
+      this.tasks.push(task);
+      this.taskRegister.reset();
+    }
   }
 
 }
